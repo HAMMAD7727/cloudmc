b3d9eb423a1ebba1094173c25fb3dc1566afa28c
mhk hammad <hamzahammad78607@gmail.com>
2025-10-11 20:00:54 +0000

    make this app ready to uplaod in vercel all check ups and .env file

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,13 @@
+// This file is DEPRECATED and will be removed.
+// Firebase configuration is now loaded from environment variables.
+// See src/firebase/index.ts for the new implementation.
+
 export const firebaseConfig = {
-  "projectId": "studio-5861978842-cd484",
-  "appId": "1:509460142631:web:de95bb48f88ca30d1dcc02",
-  "apiKey": "AIzaSyCGIpHRpou6I4NR8lizU8mD2ZnHRIUTgCU",
-  "authDomain": "studio-5861978842-cd484.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "509460142631"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
