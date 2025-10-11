186bc018ed947b36e5ef33e93a60719c7c70e655
mhk hammad <hamzahammad78607@gmail.com>
2025-10-11 20:35:02 +0000

    apiKey: "AIzaSyCGIpHRpou6I4NR8lizU8mD2ZnHRIUTgCU",   authDomain: "studio

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -10,6 +10,6 @@ export const firebaseConfig: FirebaseOptions = {
   projectId: "studio-5861978842-cd484",
   storageBucket: "studio-5861978842-cd484.appspot.com",
   messagingSenderId: "509460142631",
-  appId: "1:509460142631:web:a9bdae85876356502125f4",
+  appId: "1:509460142631:web:de95bb48f88ca30d1dcc02",
   measurementId: "G-XXXXXXXXXX" // Optional: Replace with your measurement ID if you use Analytics
 };
